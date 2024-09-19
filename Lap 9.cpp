7dc9a8098d88f20de36c5e05e8bbdc52f105526c
Kittamet Pradapwan <pradapwan19@gmail.com>
2024-09-20 03:47:00 +0700

    Add files via upload

---
+++ Lap 9.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+#include <stdio.h>
+    
+int prime(int a) {
+    if (a <= 1) {
+        return 0 ;
+    }
+    if (a <= 3) {
+        return 1 ;
+    }
+    if(a > 3) {
+         for(int j = 2 ; j < a ; j++ ) {
+            if (a % j == 0) {
+                return 0 ;
+            } else if (a == a - 1) {
+                return 1 ;
+            }
+        }
+    }
+}
+
+int main() { 
+    
+    int n ;
+    
+    printf("Enter N : " ) ;     
+    scanf("%d" , &n ) ;
+    
+    int s[n] ;
+    int i ;
+    for(int i = 0 ; i < n ; i++ ) {
+        printf("Enter value a[%d] :\n " , i ) ; 
+        scanf("%d" , &s[i] ) ;
+    }//end for value
+    
+    printf("Index : ") ;
+    for(int i = 0 ; i < n ; i++ ) {       
+        printf("%d  " , i ) ;
+    }//end for Index
+    printf("\n") ;
+    
+    printf("Array : " ) ;
+    for(int i = 0 ; i < n ; i++ ) {           
+        if(prime(s[i])) {  //true
+            printf("%d " , s[i] ) ;
+        } else {   //false
+            printf("#  ") ;
+        }
+    }//end for Array
+    
+    return 0;
+}//end function
